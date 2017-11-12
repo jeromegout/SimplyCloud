f22e2cf515079b5c275df077e29ebdcf10850c51
jerome.gout <jerome.gout@gmail.com>
2017-11-13 00:18:02 +0100

    No more redirect for http requests

--- app/src/main/java/org/jeromegout/simplycloud/Initializer.java
+++ app/src/main/java/org/jeromegout/simplycloud/Initializer.java
@@ -8,6 +8,8 @@ import net.gotev.uploadservice.okhttp.OkHttpStack;
 import org.jeromegout.simplycloud.hosts.HostManager;
 import org.jeromegout.simplycloud.hosts.free.FreeHost;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 
 
@@ -17,7 +19,15 @@ public class Initializer extends Application {
     public void onCreate() {
         super.onCreate();
         UploadService.NAMESPACE = BuildConfig.APPLICATION_ID;
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client = new OkHttpClient.Builder()
+                .followRedirects(false)
+                .followSslRedirects(true)
+                .retryOnConnectionFailure(true)
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .cache(null)
+                .build();
         UploadService.HTTP_STACK = new OkHttpStack(client);
 
         //- by default dl.free.fr is used
